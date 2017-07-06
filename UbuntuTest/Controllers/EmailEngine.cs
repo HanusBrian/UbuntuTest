4bb309da5962f15754a41709c6472e3b9b1c95d7
hanus.brian <hanus.brian@gmail.com>
2017-07-06 15:01:37 -0500

    Email Engine Initial Code

--- UbuntuTest/Controllers/EmailEngine.cs
+++ UbuntuTest/Controllers/EmailEngine.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,6 +21,30 @@ namespace UbuntuTest.Controllers
         public IActionResult New()
         {
             return View();
+        }
+
+        public IActionResult SendMail(string name, string email, string msg)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("brian.hanus@outlook.com"));
+            message.To.Add(new MailboxAddress("brian.hanus@ecolab.com"));
+            message.Subject = name;
+            message.Body = new TextPart("html")
+            {
+                Text = "From: " + name + "<br>" +
+                "Contact Information: " + email + "<br>" +
+                "Message: " + msg
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp-mail.outlook.com", 587);
+                client.Authenticate("brian.hanus@outlook.com", "Bh167471");
+                client.Send(message);
+                client.Disconnect(false);
+            }
+
+            return View("Index");
         }
     }
 }
